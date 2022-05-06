391cf9ecd0ddc82162c0cffebbaa8516a22eb129
Thomas MICHEL <tmdu13@gmail.com>
2022-05-06 13:58:27 +0200

    Plot experiment results

--- code/bandit_paper.py
+++ code/bandit_paper.py
@@ -16,18 +16,21 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.flush()
 
 class Bandit():
-    def __init__(self, nb_arm, range = [0, 1], threshold = 0.7):
+    def __init__(self, nb_arm, range = [0, 1], threshold = 0.7,means=None):
         self.nb_arm = nb_arm
-        self.means = np.random.rand(nb_arm)*(range[1]-range[0]) + range[0]
-        self.means[0] = np.random.rand()*(range[1]-threshold) + threshold
+        if means == None:
+            self.means = np.random.rand(nb_arm)*(range[1]-range[0]) + range[0]
+            self.means[0] = np.random.rand()*(range[1]-threshold) + threshold
+        else:
+            self.means = means
         #print(self.means)
     
     def pull(self,arm):
         return self.means[arm]
 
 class GaussianBandit(Bandit):
-    def __init__(self, nb_arm, range = [0, 1], threshold = 0.7, sigmas = None):
-        super().__init__(nb_arm)
+    def __init__(self, nb_arm, range = [0, 1], threshold = 0.7, means=None, sigmas = None):
+        super().__init__(nb_arm,range=range,threshold=threshold,means=means)
         if sigmas != None:
             self.sigmas = sigmas
         else:
@@ -59,7 +62,7 @@ def ucb(bandit,satisfaction_level,nb_step):
     for i in range(nb_arm,nb_step):
         # Compute confidences interval and indices
         R = np.sqrt(2*np.log(1+i*(np.log(i)**2)))
-        confidence = np.zeros(nb_arm)*R / nb_pull
+        confidence = np.ones(nb_arm)*R / np.sqrt(nb_pull)
         emp_avg = arm_rewards/nb_pull
         ucb = emp_avg + confidence
 
@@ -97,7 +100,7 @@ def algo1(bandit,satisfaction_level,nb_step):
     for i in range(nb_arm,nb_step):
         # Compute confidences interval and indices
         R = np.sqrt(2*np.log(1+i*(np.log(i)**2)))
-        confidence = np.zeros(nb_arm)*R / nb_pull
+        confidence = np.ones(nb_arm)*R / np.sqrt(nb_pull)
         emp_avg = arm_rewards/nb_pull
         ucb = emp_avg + confidence
         lcb = emp_avg - confidence
@@ -143,11 +146,11 @@ def algo3(bandit,satisfaction_level,nb_step):
     for i in range(nb_arm,nb_step):
         # Compute confidences interval and indices
         R = np.sqrt(2*np.log(1+i*(np.log(i)**2)))
-        confidence = np.zeros(nb_arm)*R / nb_pull
+        confidence = np.ones(nb_arm)*R / np.sqrt(nb_pull)
         emp_avg = arm_rewards/nb_pull
         ucb = emp_avg + confidence
         lcb = emp_avg - confidence
-        ratio = (ucb - np.max(lcb,satisfaction_level))/confidence
+        ratio = (ucb - np.maximum(lcb,satisfaction_level))/confidence
 
         max_ucb_arm = np.argmax(ucb)
         if ucb[max_ucb_arm] >= satisfaction_level:
@@ -165,64 +168,93 @@ def algo3(bandit,satisfaction_level,nb_step):
     return rewards,regrets,expectations
 
 
-def experiment_ucb(bandits,satisfaction_level,nb_step,repetition=1):
-    print("Experiment UCB for satisficing")
-    rewards,regrets,expectations,choices = [],[],[],[]
-    for bandit in progressbar(bandits):
-        for _ in range(repetition):
-            rew,reg,exp,cho = ucb_suffice(bandit, satisfaction_level, nb_step)
+def experiment(algo,bandits,satisfaction_level,nb_step,nb_repetition=1):
+    print(f"Experiment ({nb_repetition} runs)")
+    rewards,regrets,expectations= [],[],[]
+    for bandit in bandits:  # In case we want to average over multiple bandit instances
+        for _ in progressbar(range(nb_repetition)):  # Repetition of the experiment on the bandit instance
+            rew,reg,exp = algo(bandit, satisfaction_level, nb_step)
             rewards.append(rew)
             regrets.append(reg)
             expectations.append(exp)
-            choices.append(cho)
-    return np.mean(rewards,0),np.mean(regrets,0),np.mean(expectations,0),np.mean(choices,0)
+    return np.mean(rewards,0),np.mean(regrets,0),np.mean(expectations,0)   # Averaging the results
 
 
-def compute_bound_mean_sat(bandit,satisfaction_level):
-    deltas = [satisfaction_level-m for m in bandit.means]
+def compute_bound_algo3_sat(bandit,satisfaction_level): # Bound on satisficing regret of algo3 assuming there exist a satisfying arm
     R = 0
-    for d in deltas:
-        if d > 0:
-            R += d + 2/d + 2*d/(np.abs(np.max(deltas))**2)
+    delta_star = np.max(bandit.means) - satisfaction_level
+    print(delta_star)
+    for m in bandit.means:
+        if m<satisfaction_level:
+            delta = satisfaction_level - m
+            R += delta + 2/delta + 7*delta/(delta_star**2)
     return R
 
-satisfaction_level = 0.7
-nb_arm = 10
-nb_step = 5000
+
+satisfaction_level = 0.8
+nb_arm = 20
+nb_step = 1000
 nb_bandit = 1
-repetition = 50
 
-bandits = [GaussianBandit(nb_arm,threshold=satisfaction_level) for _ in range(nb_bandit)]
 
-# rewards,regrets,expectations,choices = experiment_ucb(bandits, satisfaction_level, nb_step, repetition)
-# plt.plot(np.cumsum(regrets))
-# plt.title("Satisfaction regret for UCB-Satisficing")
-# (plt.figure()
-# plt.plot(expectations)
-# plt.title("Expected reward for the chosen arm for UCB-Satisficing")
-# plt.plot(np.ones_like(expectations)*satisfaction_level))
+bandits = [GaussianBandit(nb_arm,means=[i/nb_arm for i in range(nb_arm)],sigmas=[1 for i in range(nb_arm)]) for _ in range(nb_bandit)]
+
+# Experiment with 1 run of the algorithms
+nb_repetition = 1
+rewards_ucb,regrets_ucb,expectations_ucb = experiment(ucb, bandits, satisfaction_level, nb_step, nb_repetition)
+print("Number of non-satisfying actions played : ",np.sum(expectations_ucb<satisfaction_level))
+
+rewards_algo3,regrets_algo3,expectations_algo3 = experiment(algo3,bandits, satisfaction_level, nb_step, nb_repetition)
+print("Number of non-satisfying actions played : ",np.sum(expectations_algo3<satisfaction_level))
+
+plt.plot(expectations_ucb, '.',label="UCB1")
+plt.plot(expectations_algo3, '.', label="Algorithm 3")
+plt.plot(np.ones_like(expectations_algo3)*satisfaction_level,label="Satisfaction level")
+plt.title("Expected reward of the arm played at each step")
+plt.xlabel("Time step")
+plt.ylabel("Expected reward")
+plt.legend()
 
 plt.figure()
-rewards,regrets,expectations,choices = experiment_cb_ratio(bandits, satisfaction_level, nb_step, repetition)
-plt.plot(np.cumsum(regrets))
-plt.title("Satisfaction regret for CB-Ratio")
-plt.figure()
-plt.plot(expectations)
-plt.title("Expected reward for the chosen arm for CB-Ratio")
-plt.plot(np.ones_like(expectations)*satisfaction_level)
+plt.plot(np.cumsum(regrets_ucb),label="UCB1")
+plt.plot(np.cumsum(regrets_algo3), label="Algorithm 3")
+plt.title("Satisficing regret (1 run)")
+plt.xlabel("Time step")
+plt.ylabel("Statisficing regret")
+plt.legend()
 
+plt.show()
 
-# plt.figure()
-# rewards,regrets,expectations,choices = experiment_mean_sat(bandits, satisfaction_level, nb_step, repetition)
-# plt.plot(np.cumsum(regrets))
-# plt.title("Satisfaction regret for Optimistic-Mean-Satisficing")
-# bound = compute_bound_mean_sat(bandits[0], satisfaction_level)
-# plt.plot(np.ones_like(expectations)*bound)
-# plt.yscale('linear')
-# plt.figure()
-# plt.plot(expectations)
-# plt.title("Expected reward for the chosen arm for Optimistic_Mean_Satisficing")
-# plt.plot(np.ones_like(expectations)*satisfaction_level)
+# Experiment in the realizable case
+print("Realizable case")
+nb_step = 10000
+nb_repetition = 50
+rewards_ucb,regrets_ucb,expectations_ucb = experiment(ucb, bandits, satisfaction_level, nb_step, nb_repetition)
+rewards_algo3,regrets_algo3,expectations_algo3 = experiment(algo3,bandits, satisfaction_level, nb_step, nb_repetition)
+
+plt.plot(np.cumsum(regrets_ucb),label="UCB1")
+plt.plot(np.cumsum(regrets_algo3), label="Algorithm 3")
+# Plot bound ?
+# plt.plot(np.ones_like(regrets_algo3)*compute_bound_algo3_sat(bandits[0], satisfaction_level),label="Regret bound for Algorithm 3")
+plt.title("Average satisficing regret over 50 runs\nRealizable case")
+plt.xlabel("Time step")
+plt.ylabel("Statisficing regret")
+plt.legend()
+
+plt.show()
+
+# Experiment in the unrealizable case
+print("Unrealizable case")
+satisfaction_level = 1
+rewards_ucb,regrets_ucb,expectations_ucb = experiment(ucb, bandits, satisfaction_level, nb_step, nb_repetition)
+rewards_algo3,regrets_algo3,expectations_algo3 = experiment(algo3,bandits, satisfaction_level, nb_step, nb_repetition)
+
+plt.plot(np.cumsum(regrets_ucb),label="UCB1")
+plt.plot(np.cumsum(regrets_algo3), label="Algorithm 3")
+plt.title("Average satisficing regret over 50 runs\nUnrealizable case")
+plt.xlabel("Time step")
+plt.ylabel("Statisficing regret")
+plt.legend()
 
 
 plt.show()
